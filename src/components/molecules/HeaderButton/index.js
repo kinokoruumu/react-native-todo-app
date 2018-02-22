566fe09cc0487293ada57b17da0e99cc0ac5406b
kinokoruumu <kinokoruumu416@yahoo.co.jp>
2018-02-22 17:13:46 +0900

    [add] create HeaderButton

---
+++ src/components/molecules/HeaderButton/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {TouchableOpacity, View, StyleSheet} from 'react-native'
+
+const HeaderButton = (props) => (
+	<TouchableOpacity
+		onPress={() => props.onPress()}
+	>
+		<View style={styles.buttonContainer}>
+			{props.children}
+		</View>
+	</TouchableOpacity>
+)
+
+const styles = StyleSheet.create({
+	buttonContainer: {
+		paddingHorizontal: 10,
+	}
+})
+
+export default HeaderButton
